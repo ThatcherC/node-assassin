77d4eccc95553922453fffe93495cfca5a316b8e
ThatcherC <j.thatcher.c@gmail.com>
2015-03-01 17:06:09 -0500

    Began work on SSL integration

---
+++ configs/https.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"use-https":true,
+	"options":{
+		"key": 'ssl/key.pem',
+		"crt":'ssl/cert.pem',
+		"ca": 'ssl/intermediate.ca'
+	}
+};
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 //https://scotch.io/tutorials/easy-node-authentication-setup-and-local
 
 var express = 		require('express');
+var forceSSL=		require('express-force-ssl');
 var app = 			express();
 var port =			8083;
 var mysql = 		require('mysql');
 var passport = 		require('passport');
 var flash    = 		require('connect-flash');
+var http	 = 		require('http');
+var https	 = 		require('https');
+var fs		 = 		require('fs');
 
-//var morgan = 		require('morgan');
 var cookieParser = 	require('cookie-parser');
 var bodyParser = 	require('body-parser');
 var session = 		require('express-session');
 
 var configs =		require('./configs/database.js');
+var httpsConfigs =	require('./configs/https.js');
 
 var db = mysql.createConnection(configs.DBsettings);
 db.connect();
@@ -20,12 +24,21 @@ db.connect();
 require('./configs/passport')(passport,db);
 
 // set up the express application
-//app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
+if(httpsConfigs.use-https){
+	console.log("Using SSL");
+	app.use(forceSSL);
+	var serverOptions = {
+		key: fs.readFileSync(httpsConfigs.key),
+		crt: fs.readFileSync(httpsConfigs.crt),
+		ca:  fs.readFileSync(httpsConfigs.ca)
+	};
+}
 //app.use("/assassin/static",express.static(__dirname+"/static"));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('httpsPort', 8443);
 
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
@@ -36,5 +49,12 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 require('./router.js')(app,passport,db);
 require('./gameMonitor.js')(db);
-app.listen(port);
+//app.listen(port);
 
+var server = http.createServer(app);
+server.listen(8083);
+if(httpsConfigs.use-https){
+	console.log("Creating and starting HTTPS server");
+	var secureServer = https.createServer(httpsConfigs.options, app);
+	secureServer.listen(8084);
+}
